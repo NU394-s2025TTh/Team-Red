6916634aadea87e7eed878784585e537e5ca7067
Jay Yegon <jyegon504@gmail.com>
2025-04-09 17:35:56 -0500

    merged wrapper with chatpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { DataCardContainer } from './components/DataCardContainer';
 import { AddItemButton } from './components/additem';
-import RecipeGenerator from "./components/recipegen";
 import { useUserData } from "./hooks/useUserData";
 import Chat from './components/chat';
-
 
 function App() {
   const { userData, loading, error } = useUserData("1001");
@@ -18,11 +16,11 @@ function App() {
         <>
           <DataCardContainer userData={userData} />
           <AddItemButton />
-          <Chat ingredients={userData.fridgecontents.map((item) => item.item)} />
-          <RecipeGenerator fridgecontents={userData.fridgecontents.map((item) => item.item)} />
+          <Chat ingredients={userData.fridgecontents.map(item => item.item)} />
         </>
       )}
     </div>
   );
 }
+
 export default App;
